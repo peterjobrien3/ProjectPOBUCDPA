5d188a46a25f8f6d98f25ef1160f023198ca68c9
peterjobrien3 <84069412+peterjobrien3@users.noreply.github.com>
2021-06-15 11:52:14 +0100

    Changed source file for car sales

--- main.py
+++ main.py
@@ -7,48 +7,30 @@ from pyjstat import pyjstat
 import matplotlib as mpl
 
 # Create a function to read the car sales csv's
-carsales2015=pd.read_csv("passenger cars 2015.csv",delimiter=',', quotechar='"', index_col=0)
-carsales2016=pd.read_csv("passenger cars 2016.csv",delimiter=',', quotechar='"', index_col=0)
-carsales2017=pd.read_csv("passenger cars 2017.csv",delimiter=',', quotechar='"', index_col=0)
-carsales2018=pd.read_csv("passenger cars 2018.csv",delimiter=',', quotechar='"', index_col=0)
-carsales2019=pd.read_csv("passenger cars 2019.csv",delimiter=',', quotechar='"', index_col=0)
+carsales_all=pd.read_csv("TEA18.20210615T100603.csv",delimiter=',', quotechar='"', index_col=0)
 # Check the csv files are consistent in format
-print(carsales2015.head())
-print(carsales2016.head())
-print(carsales2017.head())
-print(carsales2018.head())
-print(carsales2019.head())
-# Join the car sales csv's into one dataframe using concat
-carsales_all= pd.concat([carsales2015, carsales2016, carsales2017, carsales2018, carsales2019])
-# Check for missing values and remove
+print(carsales_all.head())
 print(carsales_all.info())
 print(carsales_all.shape)
 missing_values_count = carsales_all.isnull().sum()
-print(missing_values_count[0:5])
+print(missing_values_count[0:7])
 carsales_all.to_csv("carsales_all.csv")
-# Delete for kissing values
+# Delete for blank value
 carsales_all= carsales_all.dropna()
 print(carsales_all.shape)
-carsales_all=carsales_all.reset_index()
-print(carsales_all.head())
-print(carsales_all.dtypes)
-# Change Year column datatype to a string.
-carsales_all["Year"]=carsales_all["Year"].astype(np.int64)
-carsales_all["Year"]=carsales_all["Year"].astype(str)
-# carsales_all["Car registration count"]=carsales_all["Car registration count"].astype(int)
-print(carsales_all.dtypes)
-# Create a column called period for the month and the year.
-carsales_all["Period"]=carsales_all['Month'].astype(str) +" "+ carsales_all['Year']
-print(carsales_all.dtypes)
-print(carsales_all.head())
-carsales_all_year_by_type=carsales_all.groupby("Year")["Car registration count"].sum()
 
-print(carsales_all_year_by_type.head())
-
-# Add a line graph
+# From column Statistics Delete Rows all private Cars
+# from column Engine Capacity cc delete rows All engine capacities with respect to private cars (cc
+# From column car make delete rows all makes
+# From column emission band delete column all bands
+# From column Licensing Authority delete columns All licensing Authorities
+# Delete column number
+# Set index to county
 
 
-# Add a stacked bar chart per year.
+# Add a line graph by year and sales
 
-# carsales_all["Car registration count"]=carsales_all["Car registration count"].astype(int(float("car registration count")))
 
+# Add a stacked bar chart per year imports and new registrations
+
+# Add a diagram by county
--- population_workings.py
+++ population_workings.py
@@ -104,3 +104,57 @@ print(census_2016_pivot_dist)
 
 # What is the county with the highest % of young people, what is the county with the highest percentage of old people.
 # Extract non car buying age groups.
+
+# CAR SALES CODING REV 1
+# Create a function to read the car sales csv's
+carsales2015=pd.read_csv("passenger cars 2015.csv",delimiter=',', quotechar='"', index_col=0)
+carsales2016=pd.read_csv("passenger cars 2016.csv",delimiter=',', quotechar='"', index_col=0)
+carsales2017=pd.read_csv("passenger cars 2017.csv",delimiter=',', quotechar='"', index_col=0)
+carsales2018=pd.read_csv("passenger cars 2018.csv",delimiter=',', quotechar='"', index_col=0)
+carsales2019=pd.read_csv("passenger cars 2019.csv",delimiter=',', quotechar='"', index_col=0)
+# Check the csv files are consistent in format
+print(carsales2015.head())
+print(carsales2016.head())
+print(carsales2017.head())
+print(carsales2018.head())
+print(carsales2019.head())
+# Join the car sales csv's into one dataframe using concat
+carsales_all= pd.concat([carsales2015, carsales2016, carsales2017, carsales2018, carsales2019])
+# print(carsales_all.strip(","))
+# Check for missing values and remove
+print(carsales_all.info())
+print(carsales_all.shape)
+missing_values_count = carsales_all.isnull().sum()
+print(missing_values_count[0:5])
+carsales_all.to_csv("carsales_all.csv")
+# Delete for kissing values
+carsales_all= carsales_all.dropna()
+print(carsales_all.shape)
+carsales_all=carsales_all.reset_index()
+print(carsales_all.head())
+print(carsales_all.dtypes)
+# Change Year column datatype to a string.
+carsales_all["Year"]=carsales_all["Year"].astype(np.int64)
+carsales_all["Year"]=carsales_all["Year"].astype(str)
+carsales_all.to_csv("carsales_all_1.csv")
+# Replace '.' in the column car "registration count"
+# carsales_all["car registration count"] = carsales_all["car registration count"].str.replace(',','')
+print(carsales_all.head())
+carsales_all["Car registration count"]=carsales_all["Car registration count"].astype(int(float("car registration count")))
+# carsales_all["Car registration count"]=carsales_all["Car registration count"].astype(int)
+print(carsales_all.dtypes)
+# Create a column called period for the month and the year.
+carsales_all["Period"]=carsales_all['Month'].astype(str) +" "+ carsales_all['Year']
+print(carsales_all.dtypes)
+print(carsales_all.head())
+carsales_all_year_by_type=carsales_all.groupby("Year")["Car registration count"].sum()
+
+print(carsales_all_year_by_type.head())
+
+# Add a line graph
+
+
+# Add a stacked bar chart per year.
+
+# carsales_all["Car registration count"]=carsales_all["Car registration count"].astype(int(float("car registration count")))
+
